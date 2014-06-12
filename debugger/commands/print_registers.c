7242bcb0c80e9fc9abc56afe7a7caaefb8bfbd26
Drew DeVault <sir@cmpwn.com>
2014-06-11 21:34:19 -0600

    Improve print command

--- debugger/commands/print_registers.c
+++ debugger/commands/print_registers.c
@@ -15,6 +15,7 @@ int command_print_registers(struct debugger_state *state, int argc, char **argv)
         state->print(state, "   AF: 0x%04X   BC: 0x%04X   DE: 0x%04X  HL: 0x%04X\n", r.AF, r.BC, r.DE, r.HL);
         state->print(state, "  'AF: 0x%04X  'BC: 0x%04X  'DE: 0x%04X 'HL: 0x%04X\n", r._AF, r._BC, r._DE, r._HL);
         state->print(state, "   PC: 0x%04X   SP: 0x%04X   IX: 0x%04X  IY: 0x%04X\n", r.PC, r.SP, r.IX, r.IY);
+        state->print(state, "   IFF1: %d      IFF2: %d      IM %d\n", cpu->IFF1, cpu->IFF2, cpu->int_mode);
         state->print(state, "Flags: ");
         if (r.flags.S) state->print(state, "S ");
         if (r.flags.Z) state->print(state, "Z ");
@@ -23,8 +24,6 @@ int command_print_registers(struct debugger_state *state, int argc, char **argv)
         if (r.flags.N) state->print(state, "N ");
         if (r.flags.C) state->print(state, "C ");
         if (r.F == 0) state->print(state, "None set");
-        state->print(state, "\n");
-        state->print(state, "   IFF1: %d   IFF2: %d   IM %d", cpu->IFF1, cpu->IFF2, cpu->int_mode);
         state->print(state, "\n");
 
         return 0;
