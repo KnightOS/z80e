40f265f3462fb6912531267651ab78ee24805ee6
Puck Meerburg <puck@puckipedia.nl>
2014-06-14 13:53:31 +0200

    Fix / Improve hexdump output

--- debugger/commands/hexdump.c
+++ debugger/commands/hexdump.c
@@ -6,7 +6,7 @@
 #include <ctype.h>
 
 int command_hexdump(struct debugger_state *state, int argc, char **argv) {
-    if (argc != 3 && argc != 4) {
+    if (argc != 3) {
         state->print(state, "%s `start` `length` - print an amount of bytes from the memory\n"
                 " Prints the bytes starting from `start`, `length` bytes in total.\n", argv[0]);
         return 0;
@@ -17,45 +17,38 @@ int command_hexdump(struct debugger_state *state, int argc, char **argv) {
     uint16_t start = parse_expression(state, argv[1]);
     uint16_t length = parse_expression(state, argv[2]);
 
-    int column = 26;
-    if (argc == 4) {
-        column = atoi(argv[3]);
-    }
+    uint16_t i, total;
 
-    uint16_t i = 0;
-    for (i = 0; i < length; i++) {
-        if (i % 16 == 0) {
-            state->print(state, "0x%02X  ");
-        }
-        state->print(state, "%02X ", ti_read_byte(mmu, start + i));
-        if (i % column == 0 && i > 0) {
-            int j = 0;
-            state->print(state," |");
-            for (j = 0; j < 16; j++) {
-                char byte = ti_read_byte(mmu, start + i - 16 + j);
-                if (isprint(byte) && byte != '\t') {
-                    state->print(state, "%c", byte);
-                } else {
-                    state->print(state, ".");
-                }
-            }
-            state->print(state, "|\n");
-        }
-    }
+    while (length > 0) {
+        state->print(state, "0x%04X ", start);
 
-    if (i % 16 != 0) {
-        int over = i % 16;
-        int j;
-        state->print(state, "%*s |", 3 * (16 - over), " ");
-        for (j = 0; j < over; j++) {
-            char byte = ti_read_byte(mmu, start + i - 16 + j);
-            if (isprint(byte) && byte != '\t') {
-                state->print(state, "%c", byte);
+        for (i = 0; i < 8 && length - i > 0; i++) {
+            state->print(state, "%02X ", ti_read_byte(mmu, start + i));
+        }
+        start += i;
+        length -= i;
+        total += i;
+
+        state->print(state, " ");
+
+        for (i = 0; i < 8 && length - i > 0; i++) {
+            state->print(state, "%02X ", ti_read_byte(mmu, start + i));
+        }
+        start += i;
+        length -= i;
+        total += i;
+
+        state->print(state, "%*s|", (16 - total) * 3 + (i < 8 ? 1 : 0), " ");
+        for (i = 0; i < total; i++) {
+            char c = ti_read_byte(mmu, start - 16 + i);
+            if (isprint(c) && c != '\t') {
+                state->print(state, "%c", c);
             } else {
                 state->print(state, ".");
             }
-        }     
+        }
         state->print(state, "|\n");
+        total = 0;
     }
 
     return 0;
