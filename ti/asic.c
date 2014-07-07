295a6a4d3a3bd49145c42abbdb15bbc6867e1b35
Puck Meerburg <puck@puckipedia.nl>
2014-07-07 21:56:13 +0200

    Add mirrored ports to the Asic

--- ti/asic.c
+++ ti/asic.c
@@ -9,10 +9,38 @@
 
 void plug_devices(asic_t *asic) {
     /* Link port unimplemented */
+
     asic->cpu->devices[0x01] = init_keyboard();
     asic->cpu->devices[0x02] = init_status(asic);
 
     init_mapping_ports(asic);
+}
+
+void asic_null_write(void *ignored, uint8_t value) {
+
+}
+
+void asic_mirror_ports(asic_t *asic) {
+    int i;
+
+    switch (asic->device) {
+    case TI83p: 
+        for (i = 0x08; i < 0x10; i++) {
+            asic->cpu->devices[i] = asic->cpu->devices[i & 0x07];
+            asic->cpu->devices[i].write_out = asic_null_write;
+        }
+
+        for (i = 0x14; i < 0x100; i++) {
+            asic->cpu->devices[i] = asic->cpu->devices[i & 0x07];
+            asic->cpu->devices[i].write_out = asic_null_write;
+        }
+        break;
+
+    default:
+        for (i = 0x60; i < 0x80; i++) {
+            asic->cpu->devices[i] = asic->cpu->devices[i - 0x20];
+        }
+    }
 }
 
 void free_devices(asic_t *asic) {
@@ -32,6 +60,7 @@ asic_t *asic_init(ti_device_type type) {
     device->battery = BATTERIES_GOOD;
     device->device = type;
     plug_devices(device);
+    asic_mirror_ports(device);
     return device;
 }
 
