e1061923e741c8ada199a65b7fec639cda5fc8d4
Puck Meerburg <puck@puckipedia.nl>
2014-07-20 22:38:00 +0200

    Have main.c print_state use print_state (the function)

--- main.c
+++ main.c
@@ -301,19 +301,7 @@ int main(int argc, char **argv) {
     }
 
     if (context.print_state) {
-        z80registers_t r = device->cpu->registers;
-        printf("   AF: 0x%04X   BC: 0x%04X   DE: 0x%04X  HL: 0x%04X\n", r.AF, r.BC, r.DE, r.HL);
-        printf("  'AF: 0x%04X  'BC: 0x%04X  'DE: 0x%04X 'HL: 0x%04X\n", r._AF, r._BC, r._DE, r._HL);
-        printf("   PC: 0x%04X   SP: 0x%04X   IX: 0x%04X  IY: 0x%04X\n", r.PC, r.SP, r.IX, r.IY);
-        printf("Flags: ");
-        if (r.flags.S) printf("S ");
-        if (r.flags.Z) printf("Z ");
-        if (r.flags.H) printf("H ");
-        if (r.flags.PV) printf("P/V ");
-        if (r.flags.N) printf("N ");
-        if (r.flags.C) printf("C ");
-        if (r.F == 0) printf("None set");
-        printf("\n");
+        print_state(&device->cpu->registers);
     }
     asic_free(device);
     return 0;
