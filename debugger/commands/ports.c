5b779c2304025a835e70e599abe1f6af5f640152
Puck Meerburg <puck@puckipedia.nl>
2014-07-21 21:42:26 +0200

    Fix segfault on non-implemented port

--- debugger/commands/ports.c
+++ debugger/commands/ports.c
@@ -8,7 +8,10 @@ int command_in(struct debugger_state *state, int argc, char **argv) {
 	}
 
 	uint8_t port = parse_expression(state, argv[1]) & 0xFF;
-	uint8_t val = state->asic->cpu->devices[port].read_in(state->asic->cpu->devices[port].device);
+	uint8_t val = 0;
+	if (state->asic->cpu->devices[port].read_in != NULL) {
+		val = state->asic->cpu->devices[port].read_in(state->asic->cpu->devices[port].device);
+	}
 	state->print(state, "port 0x%02X -> 0x%02X\n", port, val);
 	return 0;
 }
@@ -21,7 +24,9 @@ int command_out(struct debugger_state *state, int argc, char **argv) {
 
 	uint8_t port = parse_expression(state, argv[1]) & 0xFF;
 	uint8_t val = parse_expression(state, argv[2]) & 0xFF;
-	state->asic->cpu->devices[port].write_out(state->asic->cpu->devices[port].device, val);
+	if (state->asic->cpu->devices[port].write_out != NULL) {
+		state->asic->cpu->devices[port].write_out(state->asic->cpu->devices[port].device, val);
+	}
 	state->print(state, "port 0x%02X <- 0x%02X\n", port, val);
 	return 0;
 }
