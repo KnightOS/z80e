0addf66dc9198579988985474c9b7906620d2f36
Puck Meerburg <puck@puckipedia.nl>
2014-08-07 23:00:39 +0200

    Fix keyboard code to work as on real hardware

--- ti/hardware/keyboard.c
+++ ti/hardware/keyboard.c
@@ -29,10 +29,6 @@ uint8_t read_keyboard(void *_state) {
     keyboard_state_t *state = (keyboard_state_t*)_state;
     uint8_t mask = state->group_mask;
 
-    if (mask == 0) {
-        return 0xFF;
-    }
-
     uint8_t value = 0;
     int i;
     for (i = 7; i >= 0; i--) {
@@ -48,10 +44,6 @@ void write_keyboard(void *_state, uint8_t value) {
     keyboard_state_t *state = (keyboard_state_t*)_state;
     if (value == 0xFF) {
         state->group_mask = 0;
-        int i;
-        for (i = 0; i < 8; i++) {
-            state->groups[i] = 0xFF;
-        }
         return;
     }
     state->group_mask |= ~value;
