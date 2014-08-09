321ec08bb6e736c84bd3ca58cc62675ea29e2bbd
Puck Meerburg <puck@puckipedia.nl>
2014-08-09 21:38:52 +0200

    print_expression: combine all args into one expression, tersify output

--- debugger/commands/print_expression.c
+++ debugger/commands/print_expression.c
@@ -1,15 +1,34 @@
 #include "commands.h"
 #include "debugger.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 int command_print_expression(struct debugger_state *state, int argc, char **argv) {
-	if (argc != 2) {
+	if (argc == 1) {
 		state->print(state, "%s `expression` - Print an expression\n Don't forget to quote your expression!\n", argv[0]);
 		return 0;
 	}
 
-	uint16_t expr = parse_expression(state, argv[1]);
+	int strl = 0;
+	int i;
+	for (i = 0; i < argc - 1; i++) {
+		strl += strlen(argv[i + 1]) + 1;
+	}
 
-	state->print(state, "The result of the expression is: 0x%04X (%u)\n", expr, expr);
+	char *data = malloc(strl);
+	char *dpointer = data;
+	for (i = 0; i < argc - 1; i++) {
+		strcpy(dpointer, argv[i + 1]);
+		dpointer += strlen(argv[i + 1]);
+		*(dpointer++) = ' ';
+	}
+	*(dpointer - 1) = 0;
 
+	uint16_t expr = parse_expression(state, data);
+
+	state->print(state, "%s = 0x%04X (%u)\n", data, expr, expr);
+
+	free(data);
 	return 0;
 }
