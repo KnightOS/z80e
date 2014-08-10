f929630f3c7dc5713fcaa0e9c8e7e5fc82b57d6d
Puck Meerburg <puck@puckipedia.nl>
2014-08-10 17:50:01 +0200

    Remove register_* helpers

--- debugger/commands.h
+++ debugger/commands.h
@@ -20,21 +20,5 @@ int command_out(debugger_state_t *state, int argc, char **argv);
 int command_break(debugger_state_t *state, int argc, char **argv);
 int command_step_over(debugger_state_t *state, int argc, char **argv);
 
-void register_hexdump(debugger_t *, const char *, int, ti_mmu_t *);
-void register_disassemble(debugger_t *, const char *, int, ti_mmu_t *);
-void register_print_registers(debugger_t *, const char *, int, z80cpu_t *);
-void register_print_expression(debugger_t *, const char *, int);
-void register_stack(debugger_t *, const char *, int, z80cpu_t *);
-void register_print_mappings(debugger_t *, const char *, int);
-void register_unhalt(debugger_t *, const char *, int, z80cpu_t *);
-void register_step(debugger_t *, const char *, int);
-void register_run(debugger_t *, const char *, int);
-void register_stop(debugger_t *, const char *, int);
-void register_on(debugger_t *, const char *, int);
-void register_in(debugger_t *, const char *, int);
-void register_out(debugger_t *, const char *, int);
-void register_break(debugger_t *, const char *, int);
-void register_step_over(debugger_t *, const char *, int);
-
 uint16_t parse_expression(debugger_state_t *, const char *);
 #endif
