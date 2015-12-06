839af2b0cab5ff10871f5e51492f4d0c3541ad8d
Jacob Young <jacobly0@users.noreply.github.com>
2015-12-06 15:35:24 -0500

    Add flash control port.

--- libz80e/src/ti/hardware/flash.c
+++ libz80e/src/ti/hardware/flash.c
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+uint8_t read_control_port(void *device) {
+	return 0;
+}
+
+void write_control_port(void *device, uint8_t data) {
+	flash_state_t *state = device;
+	// TODO: check permissions
+	state->asic->mmu->flash_unlocked = !!data;
+}
+
 uint8_t read_size_port(void *device) {
 	flash_state_t *state = device;
 	return state->chip_size;
@@ -13,7 +23,7 @@ uint8_t read_size_port(void *device) {
 
 void write_size_port(void *device, uint8_t data) {
 	flash_state_t *state = device;
-	state->chip_size = data & 0x1F;
+	state->chip_size = data & 0x33;
 }
 
 void init_flash_ports(asic_t *asic) {
@@ -23,8 +33,10 @@ void init_flash_ports(asic_t *asic) {
 	state->asic = asic;
 	state->chip_size = 0x33;
 
+	z80iodevice_t chip_control_port = { state, read_control_port, write_control_port };
 	z80iodevice_t chip_size_port = { state, read_size_port, write_size_port };
 
+	asic->cpu->devices[0x14] = chip_control_port;
 	asic->cpu->devices[0x21] = chip_size_port;
 }
 
